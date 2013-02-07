70b18d6deac0468efd54d06e10f2660f959686e5
Tim Rosenberg <timhrosenberg@gmail.com>
2013-02-07 16:49:42 -0500

    Create a Rails Application and added Status Updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beacon::Application.config.secret_token = '4e73f5293fe89d6d81b2c44a1875bf2fd140a53634ce860db429054d775a5a59b8e8f920327c0b32df89233930d0e8c0cdfc054325fc1120bb759cd249034773'
